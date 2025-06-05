bc8f5faaa98eecf4e88149163fbc00435bc0f87b
Rohit Parit <rohitparit70@gmail.com>
2025-06-05 23:28:42 +0530

    Create 2.py
    The swapping of two number

---
+++ 2.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("\nBefore swapping:")
+print("Number 1:", num1)
+print("Number 2:", num2)
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("\nAfter swapping:")
+print("Number 1:", num1)
+print("Number 2:", num2)
